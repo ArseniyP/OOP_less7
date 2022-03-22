54ad085a28bf196704e0a1ae929ebed9335ef9bd
Арсений <arseniyp91@gmail.com>
2022-03-22 11:41:11 +0200

    Update Main.java
    Test

--- src/com/phoenix/main/Main.java
+++ src/com/phoenix/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
         
         in.close();
     }
