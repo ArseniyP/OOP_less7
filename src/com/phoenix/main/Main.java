ab16d5ab23ec1c88fdb905a64bd8fd8357c7267a
Арсений <arseniyp91@gmail.com>
2022-03-22 11:25:14 +0200

    Update Main.java
    Add Scanner

--- src/com/phoenix/main/Main.java
+++ src/com/phoenix/main/Main.java
@@ -1,8 +1,13 @@
 package com.phoenix.main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello World!");
+        
+        in.close();
     }
 }
